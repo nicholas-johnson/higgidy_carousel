c32ec00a00fbc55bbc44862e266f87cc3baad1a2
Nicholas Johnson <nicholas@forwardadvance.com>
2014-12-09 15:48:19 +0000

    Higgidy Carousel

---
+++ Gruntfile.js
@@ -0,0 +1,153 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '/**\n' +
+      ' * <%= pkg.description %>\n' +
+      ' * @version v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      ' * @link <%= pkg.homepage %>\n' +
+      ' * @author <%= pkg.author.name %> - <%= pkg.author.email %>\n' +
+      ' * @license MIT License, http://www.opensource.org/licenses/MIT\n' +
+      ' */\n'
+    },
+    nodeunit: {
+      files: ['test/**/*_test.js'],
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: 'nofunc',
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        node: true,
+        devel: true,
+        globals: {
+          angular: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: ['lib/**/*.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      },
+      src: {
+        src: ['<%= dirs.src %>/*.js']
+      }
+    },
+    dirs: {
+      src: 'src',
+      dest: 'dist'
+    },
+    concat: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      dist: {
+        src: ['<%= dirs.src %>/*.js', '<%= dirs.src %>/**/*.js'],
+        dest: '<%= dirs.dest %>/<%= pkg.name %>.js'
+      }
+    },
+    autoprefixer: {
+      source: {
+        //options: {
+          //browsers: ['last 2 version']
+        //},
+        src: '<%= dirs.dest %>/<%= pkg.name %>.css',
+        dest: '<%= dirs.dest %>/<%= pkg.name %>.css'
+
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          '<%= dirs.dest %>/<%= pkg.name %>.css': '<%= dirs.src %>/<%= pkg.name %>.scss'
+        }
+      }
+    },
+    cssmin: {
+      combine: {
+        files: {
+          '<%= dirs.dest %>/<%= pkg.name %>.min.css': ['<%= dirs.dest %>/<%= pkg.name %>.css']
+        }
+      }
+    },
+    ngAnnotate: {
+      dist: {
+        files: {
+          '<%= concat.dist.dest %>': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      dist: {
+        src: ['<%= concat.dist.dest %>'],
+        dest: '<%= dirs.dest %>/<%= pkg.name %>.min.js'
+      }
+    },
+    watch: {
+      dev: {
+        files: ['<%= dirs.src %>/*'],
+        tasks: ['build', 'jshint:src'],
+        options: {
+          livereload: true,
+        },
+      },
+      html: {
+        files: 'index.html',
+        tasks: ['build', 'jshint:src'],
+        options: {
+          livereload: true,
+        },
+      },
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'nodeunit']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'nodeunit']
+      },
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-ng-annotate');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-livereload');
+
+  // Build Task
+  grunt.registerTask('build', ['jshint', 'concat', 'ngAnnotate', 'uglify', 'sass', 'autoprefixer', 'cssmin']);
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'nodeunit', 'concat', 'ngAnnotate', 'uglify']);
+
+};
---
+++ src/simple_carousel.js
@@ -0,0 +1,70 @@
+"use strict";
+
+angular.module('HiggidyCarousel',  [])
+
+  .controller('HiggidyCarousel.controller', function($scope, $interval) {
+    var timeout;
+    $scope.carousel = {
+      current: 0,
+      max: 0,
+      timeout: 10000
+    };
+    $scope.setMax = function() {
+      if ($scope.images){
+        $scope.carousel.max = $scope.images.length;
+      } else {
+        $scope.carousel.max = 1;
+      }
+    };
+    $scope.show = function(i) {
+      $scope.carousel.current = i;
+    };
+    $scope.moveOn = function() {
+      $scope.carousel.current++;
+      if ($scope.carousel.current >= $scope.carousel.max) {
+        $scope.carousel.current = 0;
+      }
+    };
+    $scope.initTimeout = function() {
+      timeout = $interval($scope.moveOn, $scope.carousel.timeout);
+    };
+    $scope.$watch('carousel.timeout', $scope.initTimeout);
+    $scope.$watch('images', $scope.setMax);
+  })
+
+  .directive('higgidyCarousel', function() {
+    var directive = {
+      controller: 'HiggidyCarousel.controller',
+      scope: true,
+      link: function(scope, element, attrs) {
+        scope.carousel.width = element[0].offsetWidth;
+        scope.getWidth = function() {
+          scope.carousel.width = element[0].offsetWidth;
+        };
+        scope.carousel.timeout = attrs.timeout || 1000;
+      }
+    };
+    return directive;
+  })
+
+  .directive('higgidyCarouselImages', function() {
+    var directive = {
+      link: function(scope, element) {
+        scope.setsWidths = function() {
+          var totalWidth = scope.carousel.width * scope.carousel.max;
+          element.find('img').css({
+            width: scope.carousel.width + 'px'
+          });
+          element.css({
+            width: totalWidth + 'px'
+          });
+        };
+        scope.animateScroll = function() {
+          element.css( {'margin-left': 0-scope.carousel.width * scope.carousel.current + "px"});
+        };
+        scope.$watch('carousel.max', scope.setsWidths);
+        scope.$watch('carousel.current', scope.animateScroll);
+      }
+    };
+    return directive;
+  });
